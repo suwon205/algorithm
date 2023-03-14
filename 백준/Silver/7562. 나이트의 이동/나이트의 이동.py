7231d0229e7b05123ceec0c772d3eba52586056b
suwonYANG <122503936+suwon205@users.noreply.github.com>
2023-03-14 14:42:27 +0900

    [Silver I] Title: 나이트의 이동, Time: 1332 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -2,33 +2,35 @@ from collections import deque
 import sys
 
 input = sys.stdin.readline
+# 방문한 곳을 1로 표시할 것. 0으로 표시되고 + 나이트의 이동 범위라면 움직일 것
 
 dx = [-1, -2, -2, -1, 1, 2, 2, 1]
 dy = [-2, -1, 1, 2, -2, -1, 1, 2]
 
 
-def bfs(arr, x, y, fx, fy):
+def bfs(x, y, wx, wy):
     q = deque()
     q.append((x, y))
-    arr[x][y] = 1
+    chess[x][y] = 1
+
     while q:
         x, y = q.popleft()
-        if x == fx and y == fy:
-            print(arr[fx][fy]-1)
+        if x == wx and y == wy:
+            print(chess[x][y]-1)
             return
-        for k in range(8):
-            nx = x + dx[k]
-            ny = y + dy[k]
-            if 0 <= nx < l and 0 <= ny < l and arr[nx][ny] == 0:
-                q.append((nx, ny))
-                arr[nx][ny] = arr[x][y] +1
+        else:
+            for k in range(8):
+                nx = x + dx[k]
+                ny = y + dy[k]
+                if 0<=nx<l and 0<=ny<l and chess[nx][ny] == 0:
+                    q.append((nx,ny))
+                    chess[nx][ny] = chess[x][y]+1
 
 T = int(input())
 for _ in range(T):
     l = int(input())
-    cnt = 0
-    arr = [[0] * l for _ in range(l)]
+    chess = [[0] * l for _ in range(l)]
     x, y = map(int, input().split())
-    fx, fy = map(int, input().split())
-    arr[x][y] = 1
-    bfs(arr, x, y, fx, fy)
+    want_x, want_y = map(int, input().split())
+    chess[x][y] = 1
+    bfs(x, y, want_x, want_y)
