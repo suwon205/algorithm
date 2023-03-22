2652621c816c8e1aa903084971f96441aeef686c
suwonYANG <122503936+suwon205@users.noreply.github.com>
2023-03-22 16:29:15 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,12 +1,12 @@
 N = int(input())
+
 lst = []
 for _ in range(N):
-    T,P = map(int,input().split())
-    lst.append((T,P))
-dp = [0 for i in range(N+1)]
-for i in range(N):
-    for k in range(i+lst[i][0],N+1):
-        if dp[k] < dp[i] + lst[i][1]:
-            dp[k] = dp[i] + lst[i][1]
+    lst.append(list(map(int, input().split())))
+DP = [0 for _ in range(N+1)]
 
-print(dp[-1])
+for i in range(N): #시작일
+    for k in range(i+lst[i][0],N+1): #시작일 다음 상담일
+        if DP[k] < DP[i] + lst[i][1]: 
+            DP[k] = DP[i] + lst[i][1]
+print(DP[-1])
