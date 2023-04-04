ff129a8dc45daf59aebd461c0ec69576f14189cf
suwonYANG <122503936+suwon205@users.noreply.github.com>
2023-04-04 22:41:59 +0900

    [Gold IV] Title: 축구, Time: 384 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1344. 축구/축구.py
+++ 백준/Gold/1344. 축구/축구.py
@@ -1,3 +1,8 @@
+# 최대 18점 득점 가능(5분당 1골, 90분 플레이)
+# 0~16에서의 소수는 2, 3, 5, 7, 11, 13, 17
+# 0~16에서의 소수가 아닌 수는 0,1,4,6,8,9,10,12,14,15,16,18
+# 적어도 한 팀이 소수로 득점할 확률 = 1 - 두 팀이 소수로 득점하지 않을 확률
+
 def percent(score, pct):
     cnt = 0
     while cnt != score: #점수를 낼 확률 구하기
@@ -18,14 +23,6 @@ def par(k):
         res[k] = 1
         par(k+1)
 
-# A = int(input())/100
-# notA = 1 - A
-# B = int(input())/100
-# notB = 1 - B
-# 최대 18점 득점 가능(5분당 1골, 90분 플레이)
-# 0~16에서의 소수는 2, 3, 5, 7, 11, 13, 17
-# 0~16에서의 소수가 아닌 수는 0,1,4,6,8,9,10,12,14,15,16,18
-# 적어도 한 팀이 소수로 득점할 확률 = 1 - 두 팀이 소수로 득점하지 않을 확률
 PN = [2, 3, 5, 7, 11, 13, 17]
 percent_list = [0]*2
 for a in range(2):
@@ -35,11 +32,9 @@ for a in range(2):
     notoccur = 1 - occur
     res = [0] * 18
     par(0)
-# print(dic)
+
     for i in dic:
         dic[i] = percent(i,1) * dic[i] # 경우의 수 * 확률. 소수가 아닌 득점을 할 확률.
     percent_list[a] = sum(dic.values())
-    # print(dic)
-    # print(percent_list)
-# if percent_list[0] == 1:
+
 print(1-percent_list[0]*percent_list[1])
