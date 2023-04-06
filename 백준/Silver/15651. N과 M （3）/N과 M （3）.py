5461e73961dbfdd89dd77b25d7d1b8c7f52b1a5d
suwonYANG <122503936+suwon205@users.noreply.github.com>
2023-04-06 22:25:24 +0900

    [Silver III] Title: N과 M (3), Time: 616 ms, Memory: 123484 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,15 +1,13 @@
-N,M = map(int, input().split())
-res = []
-#이 문제의 포인트는 같은 수를 출력해도 골라도 된다는 것! 1~3의 수 중 2개의 수를 고를 때 2,2의 출력이 가능함!
-def dfs(k):
-    if k==M: #다 돈 경우
+def comb(k):
+    if k == M:
         print(' '.join(map(str,res)))
-        return
-
     else:
-        for i in range(1,N+1):
-            res.append(i)
-            dfs(k+1)
-            res.pop()
+        for i in range(N):
+            res[k] = lst[i]
+            comb(k+1)
 
-dfs(0)
+
+N,M = map(int, input().split())
+lst = [i+1 for i in range(N)]
+res = [0] * M
+comb(0)
