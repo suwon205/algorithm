2d2ebddec5af150aa93609751982e722fb5f666f
suwonYANG <122503936+suwon205@users.noreply.github.com>
2023-05-10 20:48:49 +0900

    [Gold II] Title: 뉴스 전하기, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.py
+++ 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.py
@@ -1,19 +1,18 @@
 def DP(v):
-    child = []
-    for idx in tree[v]:
-        DP(idx) #제일 끝까지 들어가기...
-        child.append(time[idx])
-    if not time[v]:
-        child.append(0)
-    child.sort(reverse=True) #시간이 많이 걸리는 사람부터 볼 것
-    nTime = [child[i] + i +1 for i in range(len(child))]
-    time[v] = max(nTime)
+    sub = [] # 노드 v를 루트로 하는 서브트리를 도는 동안 걸리는 최소 시간 구하는 리스트
+    for i in TREE[v]:
+        DP(i)
+        sub.append(time[i])
+    if not time[v]: #말단까지 온 경우!
+        sub.append(0)
+    sub.sort(reverse=True)
+    tmp_time = [sub[i] + i + 1 for i in range(len(sub))]
+    time[v] = max(tmp_time)
 N = int(input())
-tree = [[] for _ in range(N)]
-lst = list(map(int, input().split()))
-for idx in range(1,N):
-    tree[lst[idx]].append(idx)
+tmp = list(map(int, input().split()))
+TREE = [[] for _ in range(N)]
+for i in range(1,N):
+    TREE[tmp[i]].append(i)
 time = [False] * N
-
 DP(0)
 print(time[0]-1)
