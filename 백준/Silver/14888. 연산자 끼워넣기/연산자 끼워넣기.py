496d8ba85c0a4a96f05b8a03d9f97d79d647d199
suwonYANG <122503936+suwon205@users.noreply.github.com>
2023-06-21 14:28:44 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 64 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,27 +1,21 @@
 N = int(input())
-ls = list(map(int,input().split()))
+number = list(map(int, input().split()))
 pl, mi, mul, div = map(int, input().split())
-maxV = -99999999999999999
-minV = 9999999999999999
-
-
-def solution(num, idx, add, sub, mul, div):
-    global maxV, minV
+minV = 9999999999999
+maxV = -9999999999999
+def calc(tmp, idx, pl, mi, mul, div):
+    global minV, maxV
     if idx == N:
-        maxV = max(maxV, num)
-        minV = min(minV, num)
-        return
-
-    if add > 0:
-        solution(num + ls[idx], idx + 1, add - 1, sub, mul, div)
-    if sub > 0:
-        solution(num - ls[idx], idx + 1, add, sub - 1, mul, div)
-    if mul > 0:
-        solution(num * ls[idx], idx + 1, add, sub, mul - 1, div)
-    if div > 0:
-        solution(int(num / ls[idx]), idx + 1, add, sub, mul, div - 1)
-
-
-solution(ls[0], 1, pl, mi, mul, div)
+        maxV = max(maxV, tmp)
+        minV = min(minV, tmp)
+    if pl>0:
+        calc(tmp + number[idx], idx+1, pl-1, mi, mul, div)
+    if mi>0:
+        calc(tmp - number[idx], idx+1, pl, mi-1, mul, div)
+    if mul>0:
+        calc(tmp * number[idx], idx+1, pl, mi, mul-1, div)
+    if div>0:
+        calc(int(tmp / number[idx]), idx+1, pl, mi, mul, div-1)
+calc(number[0],1,pl, mi, mul, div)
 print(maxV)
 print(minV)
