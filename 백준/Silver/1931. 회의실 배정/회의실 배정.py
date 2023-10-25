d85af43886ea93942961de7f2c437b40a960f912
Suwon Yang <122503936+suwon205@users.noreply.github.com>
2023-10-25 23:51:17 +0900

    [Silver I] Title: 회의실 배정, Time: 380 ms, Memory: 121908 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,9 +1,17 @@
 N = int(input())
-lst = [list(map(int, input().split())) for _ in range(N)]
-cnt = 0
-lst.sort(key=lambda x : (x[1], x[0]))
-tmp = [lst[0]]
-for i in range(1,N):
-    if tmp[-1][1] <= lst[i][0]:
-        tmp.append(lst[i])
-print(len(tmp))
+lessons = []
+for _ in range(N):
+    st, ed = map(int, input().split())
+    lessons.append((st, ed))
+
+lessons.sort(key= lambda x : (x[1], x[0]))
+
+select_meeting = lessons[0]
+cnt = 1
+
+for i in range(1, N):
+    if lessons[i][0] >= select_meeting[1]:
+        select_meeting = lessons[i]
+        cnt += 1
+
+print(cnt)
