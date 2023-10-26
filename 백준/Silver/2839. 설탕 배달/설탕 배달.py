52efe5b96aa5368575a1c073c7e35fcc2653eda0
Suwon Yang <122503936+suwon205@users.noreply.github.com>
2023-10-26 10:04:00 +0900

    [Silver IV] Title: 설탕 배달, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,21 +1,19 @@
-# 입력 받기
 N = int(input())
 
-# 5킬로그램 봉지로 최대한 채우는 경우를 고려
-num_5kg = N // 5
-num_3kg = 0
+sugar_5 = N // 5
+sugar_3 = 0
 N %= 5
 
-# 남은 설탕을 3킬로그램 봉지로 채우는 경우를 고려
-while num_5kg >= 0:
+while sugar_5 >= 0:
     if N % 3 == 0:
-        num_3kg = N // 3
-        N -= 3 * num_3kg
+        sugar_3 = (N // 3)
+        N -= 3 * sugar_3
         break
-    num_5kg -= 1
+
+    sugar_5 -= 1
     N += 5
-# 결과 출력
-if N == 0:
-    print(num_5kg + num_3kg)
+
+if N != 0:
+    print(-1)
 else:
-    print(-1)  # 정확하게 N킬로그램을 만들 수 없는 경우
+    print(sugar_5 + sugar_3)
