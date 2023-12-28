66659ba45372fab1a18db4fa6a832439794f0adf
Soowon Yang <122503936+suwon205@users.noreply.github.com>
2023-12-28 13:44:53 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.07 ms
+메모리: 33.5 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 6일 18:0:2
+2023년 12월 4일 13:44:52
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,35 +1,40 @@
+var dict = {
+  "zero": 0,
+  "one": 1,
+  "two": 2,
+  "three": 3,
+  "four": 4,
+  "five": 5,
+  "six": 6,
+  "seven": 7,
+  "eight": 8,
+  "nine": 9
+};
+
+function isNumber(char) {
+  var numericPattern = /^[0-9]$/;
+  return numericPattern.test(char);
+}
+
 function solution(s) {
-    // 숫자에 대응되는 영단어와 그에 해당하는 숫자를 매핑
-    const wordToNumber = {
-        "zero": 0,
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9
-    };
+  var answer = '';
+  var idx = 0;
 
-    let result = "";
-    let temp = "";
-
-    for (let i = 0; i < s.length; i++) {
-        if (isNaN(s[i])) {
-            // 문자가 숫자가 아니면 temp에 추가
-            temp += s[i];
-            if (wordToNumber[temp] !== undefined) {
-                // temp가 숫자에 해당하는 영단어인 경우, 숫자로 변환하여 result에 추가
-                result += wordToNumber[temp];
-                temp = ""; // temp 초기화
-            }
-        } else {
-            // 문자가 숫자인 경우, 그대로 result에 추가
-            result += s[i];
+  while (idx < s.length) {
+    if (isNumber(s[idx])) {
+      answer += s[idx];
+      idx += 1;
+    } else {
+      for (let ed = idx + 1; ed <= s.length; ed++) {
+        var temp = s.slice(idx, ed);
+        if (dict[temp] !== undefined) {
+          answer += dict[temp];
+          idx = ed;
+          break;
         }
+      }
     }
+  }
 
-    return parseInt(result, 10); // 결과 문자열을 숫자로 변환하여 반환
+  return parseInt(answer);
 }
