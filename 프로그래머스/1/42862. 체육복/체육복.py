3c34bc426684e34663059a387fc01bebdebf7327
Soowon Yang <122503936+suwon205@users.noreply.github.com>
2024-01-04 00:06:18 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,8 +1,25 @@
 def solution(n, lost, reserve):
     answer = 0
-    for idx in range(len(reserve)):
-        if reserve[idx] in lost:
-            print(reserve, "잃었어요!")
-            reserve.pop(idx)
-        print(reserve[idx])
+    students = [1] * n
+    
+    lost_set = list(set(lost) - set(reserve)) # 진짜로 잃어버린 경우만 남도록
+    reserve_set = list(set(reserve) - set(lost)) # 진짜로 여분이 있는 경우만 남도록
+    
+    for idx in lost_set:
+        students[idx - 1] -= 1
+    for idx in reserve_set:
+        students[idx - 1] += 1
+    
+    for idx in range(n):
+        if students[idx] == 0:
+            if idx > 0 and students[idx - 1] >= 2:
+                students[idx] += 1
+                students[idx - 1] -= 1
+            elif idx < n - 1 and students[idx + 1] >= 2:
+                students[idx] += 1
+                students[idx + 1] -= 1
+
+    for cloth in students:
+        if cloth:
+            answer += 1
     return answer
