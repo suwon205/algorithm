36ffb034df69f95a58fe8701d707b2a8ce6bb765
Soowon Yang <122503936+suwon205@users.noreply.github.com>
2024-01-04 16:58:22 +0900

    [Gold V] Title: 적록색약, Time: 192 ms, Memory: 115176 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,41 +1,42 @@
 from collections import deque
+dir = [(0,1),(0,-1),(-1,0),(1,0)]
 
-dir = [(0,-1),(0,1),(-1,0),(1,0)]
-
-def bfs(r,c):
+def area(r,c):
     q = deque()
-    q.append((r,c))
     visited[r][c] = True
+    q.append((r,c))
     while q:
-        r,c = q.popleft()
-
+        r, c = q.popleft()
         for k in range(4):
-            nr = dir[k][0] + r
-            nc = dir[k][1] + c
-            if 0<=nr<N and 0<=nc<N and not visited[nr][nc]:
-                if arr[r][c] == arr[nr][nc]:
-                    visited[nr][nc] = True
-                    q.append((nr,nc))
+            nr = r + dir[k][0]
+            nc = c + dir[k][1]
+            if 0 <= nr < N and 0<= nc < N and not visited[nr][nc] and pic[nr][nc] == pic[r][c]:
+                visited[nr][nc] = True
+                q.append((nr, nc))
+    return
 
 N = int(input())
-arr = [list(input()) for _ in range(N)]
-visited = [[False]*N for _ in range(N)]
-abnormal = normal = 0
+pic = [list(input()) for _ in range(N)]
+visited = [[False] * N for _ in range(N)]
+nor_cnt = ab_cnt = 0
+# 비장애인의 경우
 for r in range(N):
     for c in range(N):
-        if visited[r][c] == False:
-            bfs(r,c)
-            normal += 1
+        if not visited[r][c]:
+            area(r,c)
+            nor_cnt += 1
+# 그림 바꾸기
 for r in range(N):
     for c in range(N):
-        if arr[r][c] == 'R':
-            arr[r][c] = 'G'
-visited = [[False]*N for _ in range(N)]
+        if pic[r][c] == 'R':
+            pic[r][c] = 'G'
 
+visited = [[False] * N for _ in range(N)]
+#색맹의 경우
 for r in range(N):
     for c in range(N):
-        if visited[r][c] == False:
-            bfs(r,c)
-            abnormal += 1
+        if not visited[r][c]:
+            area(r,c)
+            ab_cnt += 1
 
-print(normal, abnormal)
+print(nor_cnt, ab_cnt)
