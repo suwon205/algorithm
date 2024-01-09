85b4090dc2c37cf20d9d194d7a379ff6c45194e9
Soowon Yang <122503936+suwon205@users.noreply.github.com>
2024-01-09 21:21:20 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.87 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [카카오 인턴] 키패드 누르기 - 67256 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.43 ms
+메모리: 10.2 MB, 시간: 0.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 0일 19:29:29
+2024년 1월 2일 21:21:19
 
 ### 문제 설명
 
--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,43 +1,38 @@
 def solution(numbers, hand):
     answer = ''
+    keypad = {
+        1 : [0, 0], 2 : [0, 1], 3 : [0, 2],
+        4 : [1, 0], 5 : [1, 1], 6 : [1, 2],
+        7 : [2, 0], 8 : [2, 1], 9: [2, 2],
+        '*' : [3, 0], 0 : [3, 1], '#' : [3, 2]
+    }
+    # 초반 손의 위치 설정
+    leftPos = keypad['*']
+    rightPos = keypad['#']
     
-    dic = {1: [0, 0], 2: [0, 1], 3: [0, 2],
-       4: [1, 0], 5: [1, 1], 6: [1, 2],
-       7: [2, 0], 8: [2, 1], 9: [2, 2],
-       '*':[3, 0], 0: [3, 1], '#': [3, 2]}
-    
-    leftPos = dic['*']
-    rightPos = dic['#']
-    
-    for number in numbers:
-        curPos = dic[number]
-        
-        if number == 1 or number == 4 or number == 7:
-            answer += "L"
-            leftPos = curPos
-        elif number == 3 or number == 6 or number == 9:
-            answer += "R"
-            rightPos = curPos
+    leftKey = [1, 4, 7]
+    rightKey = [3, 6, 9]
+    for idx in range(len(numbers)):
+        if numbers[idx] in leftKey:
+            answer += 'L'
+            leftPos = keypad[numbers[idx]]
+        elif numbers[idx] in rightKey:
+            answer += 'R'
+            rightPos = keypad[numbers[idx]]
         else:
-            leftDistance = 0
-            rightDistance = 0
-            
-            for a, b, c in zip(leftPos, rightPos, curPos):
-                leftDistance += abs(a-c)
-                rightDistance += abs(b-c)
-            
-            if leftDistance < rightDistance:
-                answer += "L"
-                leftPos = curPos
-                
-            elif leftDistance > rightDistance:
-                answer += "R"
-                rightPos = curPos
-            else:
-                if hand == "right":
-                    answer += "R"
-                    rightPos = curPos
+            rightDis = abs(rightPos[0] - keypad[numbers[idx]][0]) + abs(rightPos[1] - keypad[numbers[idx]][1])
+            leftDis = abs(leftPos[0] - keypad[numbers[idx]][0]) + abs(leftPos[1] - keypad[numbers[idx]][1])
+            if rightDis > leftDis:
+                answer += 'L'
+                leftPos = keypad[numbers[idx]]
+            elif rightDis < leftDis:
+                answer += 'R'
+                rightPos = keypad[numbers[idx]]
+            else: # 양손에서부터의 길이가 같은 경우
+                if hand == 'right':
+                    answer += 'R'
+                    rightPos = keypad[numbers[idx]]
                 else:
-                    answer += "L"
-                    leftPos = curPos
+                    answer += 'L'
+                    leftPos = keypad[numbers[idx]]
     return answer
