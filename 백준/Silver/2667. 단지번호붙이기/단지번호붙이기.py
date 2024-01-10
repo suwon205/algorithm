eb81fcc07d75b0ba0be87bb717543516b4c468f1
Soowon Yang <122503936+suwon205@users.noreply.github.com>
2024-01-10 18:43:44 +0900

    [Silver I] Title: 단지번호붙이기, Time: 144 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,33 +1,33 @@
 from collections import deque
 
-dir = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-
+dir = [[0, 1], [0, -1], [-1, 0], [1, 0]]
 
 def bfs(r, c):
-    cnt = 1
     q = deque()
     q.append((r, c))
-    arr[r][c] = 0
+    cnt = 1  # cnt를 1로 초기화
+    houses[r][c] = 0  # 방문한 집을 0으로 표시
     while q:
         r, c = q.popleft()
         for k in range(4):
             nr = r + dir[k][0]
             nc = c + dir[k][1]
-            if 0<=nr<N and 0<=nc<N and arr[nr][nc] == 1:
+            if 0 <= nr < N and 0 <= nc < N and houses[nr][nc]:
+                houses[nr][nc] = 0
                 cnt += 1
-                arr[nr][nc] = 0
                 q.append((nr, nc))
     return cnt
 
-
 N = int(input())
-arr = [list(map(int, input())) for _ in range(N)]
-tmp = []
+houses = [list(map(int, input())) for _ in range(N)]
+cntDanzi = 0
+cntHouse = []
 for r in range(N):
     for c in range(N):
-        if arr[r][c] == 1:  # 집이라면
-            tmp.append(bfs(r, c))
-tmp.sort()
-print(len(tmp))
-for i in range(len(tmp)):
-    print(tmp[i])
+        if houses[r][c]:
+            cntHouse.append(bfs(r, c))
+            cntDanzi += 1
+print(cntDanzi)
+cntHouse.sort()
+for i in cntHouse:
+    print(i)
