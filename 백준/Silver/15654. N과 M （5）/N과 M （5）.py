7f63fb49da7447bc35ae79afc040f3b9c1bf7caa
Soowon Yang <122503936+suwon205@users.noreply.github.com>
2024-01-21 17:55:25 +0900

    [Silver III] Title: N과 M (5), Time: 192 ms, Memory: 112744 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,19 +1,19 @@
-def per(k):
-    if k == M:
-        for i in range(M):
-            print(res[i], end=' ')
+def backtracking(st):
+    if st == M:
+        for i in res:
+            print(i, end=' ')
         print()
     else:
-        for i in range(N):
-            if not used[i]:
-                res[k] = lst[i]
-                used[i] = True
-                per(k+1)
-                used[i] = False
+        for idx in range(N):
+            if not used[idx]:
+                res[st] = lst[idx]
+                used[idx] = True
+                backtracking(st + 1)
+                used[idx] = False
 
-N,M = map(int, input().split())
-lst = list(map(int, input().split()))
+N, M = map(int, input().split())
+lst = list(map(int,input().split()))
 lst.sort()
-res = [0]*M
-used = [False] * N
-per(0)
+res = [0] * M
+used = [False] * (N+1)
+backtracking(0)
