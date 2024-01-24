902665fe35cce520bb259c8cd596e14e23504c2d
Soowon Yang <122503936+suwon205@users.noreply.github.com>
2024-01-24 12:20:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.08 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
 
 ### 성능 요약
 
-메모리: 58.9 MB, 시간: 178.53 ms
+메모리: 26.3 MB, 시간: 70.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 12:14:1
+2024년 1월 3일 12:19:59
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,23 +1,9 @@
 def solution(participant, completion):
     answer = ''
-    #참여자 명단에만 있거나, 참여자 명단에서의 수가 많으면 된다.
-    pDict = {} #참여자 딕셔너리
-    cDict = {} #완주자 딕셔너리
-    for pIdx in range(len(participant)):
-        if participant[pIdx] not in pDict:
-            pDict[participant[pIdx]] = 1
-        else:
-            pDict[participant[pIdx]] += 1
+    participant.sort()
+    completion.sort()
     
-    for cIdx in range(len(completion)):
-        if completion[cIdx] not in cDict:
-            cDict[completion[cIdx]] = 1
-        else:
-            cDict[completion[cIdx]] += 1
-    for p in pDict:
-        if p not in cDict: #tc 1의 경우. 참여자에는 있으나 완주자에는 없는 경우
-            return p
-        else:
-            if pDict[p] != cDict[p]:
-                return p
+    for idx in range(len(participant)):
+        if idx == len(participant) -1 or participant[idx] != completion[idx]:
+            return participant[idx]
     return answer
