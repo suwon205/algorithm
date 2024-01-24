6814b5c05e66b324261ebebe848bcaea3c94a86e
Soowon Yang <122503936+suwon205@users.noreply.github.com>
2024-01-24 12:14:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 178.53 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,31 +1,12 @@
 function solution(participant, completion) {
-    var answer = '';
-    pDict = {}
-    cDict = {}
-    
-    for (i = 0; i < participant.length; i++) {
-        if (!(participant[i] in pDict)) {
-            pDict[participant[i]] = 1
-        } else {
-            pDict[participant[i]] += 1
+    participant.sort();
+    completion.sort();
+
+    for (var i = 0; i < participant.length; i++) {
+        if (i === participant.length - 1 || participant[i] !== completion[i]) {
+            return participant[i];
         }
     }
-    
-    for (i = 0; i < completion.length; i++) {
-        if (!(completion[i] in cDict)) {
-            cDict[completion[i]] = 1
-        } else {
-            cDict[completion[i]] += 1
-        }
-    }
-    for (p in pDict) {
-        if (!(p in cDict)) {
-            return p
-        } else {
-            if (pDict[p] !== cDict[p]) {
-                return p
-            }
-        }
-    }
-    return answer;
+
+    return ''; // 이 부분은 실행되지 않을 것이므로 빈 문자열 반환
 }
