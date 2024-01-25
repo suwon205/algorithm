26778f3d6b9baa67d1bae7e1ed5220cc958dc537
Soowon Yang <122503936+suwon205@users.noreply.github.com>
2024-01-25 16:53:30 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/README.md
+++ 프로그래머스/1/131128. 숫자 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 짝꿍 - 131128 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 2.55 ms
+메모리: 33.4 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 16:25:14
+2024년 1월 4일 16:53:29
 
 ### 문제 설명
 
--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -2,7 +2,8 @@ function solution(X, Y) {
     let xSet = new Set(X);
     let ySet = new Set(Y);
     let commonDigits = Array.from(xSet).filter(digit => ySet.has(digit));
-    console.log(commonDigits)
+    // commonDigits에는 두 문자열에 공통으로 나타나는 숫자들이 배열로 저장
+
     if (commonDigits.length === 0) {
         return '-1';
     }
@@ -12,11 +13,13 @@ function solution(X, Y) {
 
     commonDigits.sort((a, b) => b - a);
     let result = '';
-
+    // 공통으로 나타나는 숫자들을 순회하면서 최소 등장 횟수만큼 추가
     for (let digit of commonDigits) {
-        let countInX = X.split(digit).length - 1;
-        let countInY = Y.split(digit).length - 1;
+        let countInX = X.split(digit).length - 1;  // X에서 현재 숫자가 등장하는 횟수
+        let countInY = Y.split(digit).length - 1;  // Y에서 현재 숫자가 등장하는 횟수
         let minCount = Math.min(countInX, countInY);
+
+        // 최소 등장 횟수만큼 현재 숫자를 결과 문자열에 추가
         result += digit.repeat(minCount);
     }
 
