b854a667b6f013fd57f99c8da2cc9a0cc3a529a5
Soowon Yang <122503936+suwon205@users.noreply.github.com>
2024-03-05 13:12:04 +0900

    [level 2] Title: 타겟 넘버, Time: 2.84 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
 
 ### 성능 요약
 
-메모리: 36.5 MB, 시간: 18.88 ms
+메모리: 10.1 MB, 시간: 2.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 0일 14:16:53
+2024년 03월 05일 13:12:03
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,4 +1,20 @@
 function solution(numbers, target) {
     var answer = 0;
+    
+    function findTarget(idx, curSum) {
+        if (idx === numbers.length) {
+            if (curSum === target) {
+                answer++;
+            }
+            return;
+        }
+            findTarget(idx + 1, curSum + numbers[idx])
+    findTarget(idx + 1, curSum - numbers[idx])
+    }
+    
+
+    
+    findTarget(0,0)
+    
     return answer;
 }
