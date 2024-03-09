90506f5df3da3ae7960220da848c690d39510a08
Soowon Yang <122503936+suwon205@users.noreply.github.com>
2024-03-09 18:12:11 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.31 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,29 +1,28 @@
 from collections import deque
 
-dir = [(0,1),(0,-1),(1,0),(-1,0)]
+dir = [[0,1], [0,-1], [1,0], [-1,0]]
 
-def bfs(r,c, maps):
-    cnt = 1 #이동할 수 적어주기
-    limitR = len(maps)
-    limitC = len(maps[0])
-    # maps[r][c] = -1 #방문한 곳은 -1로 표시해주기
-    queue = deque()
-    queue.append((r,c))
-    while queue:
-        r, c = queue.popleft()
+def maze(r, c, arr):
+    q = deque()
+    q.append((r,c))
+    while q:
+        r,c = q.popleft()
         for k in range(4):
             nr = r + dir[k][0]
             nc = c + dir[k][1]
-            if 0<=nr<limitR and 0<=nc<limitC and maps[nr][nc] == 1: #이동 가능
-                queue.append((nr,nc))
-                maps[nr][nc] += maps[r][c] #방문함
-                cnt += 1
-                # print(nr, nc, maps)
-        
-    return maps[-1][-1]
+            if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == 1:
+                arr[nr][nc] = arr[r][c] + 1
+                q.append((nr, nc))
+    return arr
+
 
 def solution(maps):
-    answer = bfs(0,0, maps)
-    if answer == 1:
+    answer = 0
+    global N
+    global M
+    N = len(maps)
+    M = len(maps[0])
+    maps = maze(0, 0, maps)
+    if maps[-1][-1] == 1:
         return -1
-    return answer
+    return maps[-1][-1]
