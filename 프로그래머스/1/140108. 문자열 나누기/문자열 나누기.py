8d6da61a169c6ee05c396d4ad39479ecbdb6654b
Soowon Yang <122503936+suwon205@users.noreply.github.com>
2024-07-14 16:31:58 +0900

    [level 1] Title: 문자열 나누기, Time: 1.51 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,22 +1,12 @@
 def solution(s):
     answer = 0
     notX = isX = 0
-    x = ""
-    for alp in s:
-        
-        if not x:
-            x = alp
-            isX += 1
-            continue
-        if x == alp:
+    for i in s:
+        if notX == isX:
+            answer += 1
+            k = i
+        if k ==i:
             isX += 1
         else:
             notX += 1
-            
-        if isX == notX:
-            x = ""
-            answer += 1
-        print(x)
-    if x != "":
-        answer += 1
     return answer
